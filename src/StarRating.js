710e9f7e47ebe445f9f3c010aa265ed2e7a2bc2b
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-12 14:44:56 -0400

    finished static development of project

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,8 +1,19 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = { display: "flex", alignItems: "center", gap: "1rem" };
 
 const starContainerStyle = { display: "flex" };
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onMovieRating: PropTypes.func,
+};
 
 export default function StarRating({
   maxRating = 5,
@@ -18,7 +29,7 @@ export default function StarRating({
 
   function handleRating(rating) {
     setRating(rating);
-    onMovieRating(rating)
+    onMovieRating(rating);
   }
 
   const textStyle = {
