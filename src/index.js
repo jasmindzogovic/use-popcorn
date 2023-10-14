770a7deed9bac819c4a8a71d9896c019bb7eff53
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-10-14 13:57:50 -0400

    added sprint 2 to project with useEffect hooks

--- src/index.js
+++ src/index.js
@@ -1,31 +1,24 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 
 import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onMovieRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onMovieRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={10} defaultRating={3} />
-    <StarRating
-      size="2rem"
-      color="red"
-      className="test"
-      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <Test />
+    <App />
+    {/* <Test /> */}
   </React.StrictMode>
 );
